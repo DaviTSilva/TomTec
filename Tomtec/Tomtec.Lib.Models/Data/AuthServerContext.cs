dbec033f022a246cb78dde275b95d4f9bf236ab6
DaviTSilva <davithomazellaes@gmail.com>
2021-12-05 18:34:52 -0300

    API and Lib creation

---
+++ Tomtec/Tomtec.Lib.Models/Data/AuthServerContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tomtec.Lib.Models;
+
+namespace Tomtec.Lib.Models.Data
+{
+    public class AuthServerContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AuthServerContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserRoles>().HasKey(sc => new { sc.UserId, sc.RoleId });
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Address { get; set; }
+    }
+}
---
+++ Tomtec/Tomtec.Lib.Models/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Tomtec.Lib.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string AdditionalInformation { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string CountryName { get; set; }
+    }
+}
